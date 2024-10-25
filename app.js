6c423ce8a33634e37d52276e23a71e583e1d42db
dee-res <restrepo.diana.pat@gmail.com>
2024-10-25 15:20:51 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+(function() {
+  'use strict';
 
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController);
+
+  ToBuyController.$inject = ['$scope'];
+  function ToBuyController($scope) {
+    $scope.
