6dbf813d49a5e9eaac4f2e7016fc2f3e1ff5755f
dee-res <restrepo.diana.pat@gmail.com>
2024-10-25 18:57:34 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,16 @@
   angular.module('ShoppingListCheckOff', [])
   .controller('ToBuyController', ToBuyController)
   .controller('AlreadyBoughtController', AlreadyBoughtController)
-  .factory('ShoppingListFactory' ShoppingListFactory;
+  .factory('ShoppingListCheckOffService' ShoppingListCheckOffService;
 
   //To Buy Controller
-  ToBuyController.$inject = ['ShoppingListFactory'];
-  function ToBuyController(ShoppingListFactory) {
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService) {
+    var service = ShoppingListCheckOffService;
+    var ctrl = this;
+    ctrl.list = service.getToBuyList();
+        ctrl.isEmpty = service.isToBuyListEmpty;
+        ctrl.checkOff = service.checkOff;
+    }
+
     
