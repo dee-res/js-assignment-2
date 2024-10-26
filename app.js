9e07a0e8d6ebe2665aaf6e86aa438bc633d0b856
dee-res <restrepo.diana.pat@gmail.com>
2024-10-25 19:20:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
   .controller('AlreadyBoughtController', AlreadyBoughtController)
   .factory('ShoppingListCheckOffService' ShoppingListCheckOffService;
 
-  //To Buy Controller
+  // To Buy Controller
   ToBuyController.$inject = ['ShoppingListCheckOffService'];
   function ToBuyController(ShoppingListCheckOffService) {
     var service = ShoppingListCheckOffService;
@@ -15,5 +15,38 @@
         ctrl.isEmpty = service.isToBuyListEmpty;
         ctrl.checkOff = service.checkOff;
     }
+  
+  // Already Bought Controller
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var service = ShoppingListCheckOffService;
+    var ctrl = this;
+    ctrl.list = service.getBoughtList();
+    ctrl.isEmpty = service.isBoughtListEmpty;
+  }
 
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    service.toBuyList = [
+{
+      name: "Cookies"
+      quantity: 10
+  },
+ {
+      name: "Oreos"
+      quantity: 10
+}
+];
+
+service.boughtList = [];
+
+service.checkOff = function (itemIndex) {
+  var boughtItem = service.toBuyList.splice(itemIndex, 1) [0];
+  service.boughtList.push(boughtItem);
+};
+
+service.getToBuyList = function() {
+  
+      
     
